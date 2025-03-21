ab6a3b420ebe2c13ec1d0466a0705fb991844c3f
Clariza Look <clariza.look@navitas.com>
2025-03-21 18:07:11 +0800

    Fixing outputs tf file with acr name

--- infra/terraform/acr.tf
+++ infra/terraform/acr.tf
@@ -5,20 +5,3 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
-
-output "acr_name" {
-  value = var.acr_name
-}
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -3,5 +3,18 @@ output "resource_group_name" {
 }
 
 output "acr_name" {
-  value = azurerm_container_registry.acr.name
+  value = var.acr_name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
