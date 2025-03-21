c6f8d244cbdf9f5944e7b00f284fb2b0dbc12bdb
Clariza Look <clariza.look@navitas.com>
2025-03-21 18:01:13 +0800

    Fixing outputs tf file with acr name

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,28 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-/*
-output "app_service_plan_name" {
-  value = azurerm_service_plan.asp.name
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
 }
-
-output "app_service_name" {
-  value = azurerm_linux_web_app.app.name
-}
-
-output "app_service_hostname" {
-  value = azurerm_linux_web_app.app.default_hostname
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
-
-output "storage_container_name" {
-  value = azurerm_storage_container.sc.name
-}
-
-output "storage_account_primary_blob_endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
-}
-*/
