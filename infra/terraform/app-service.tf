b9e909dd61cd841fd6d453577b12cc29076b60c0
Clariza Look <clarizlook.tech@gmail.com>
2025-04-15 23:30:46 +0800

    Fixing app-service.tf file to deploy app service in azure

--- infra/terraform/app-service.tf
+++ infra/terraform/app-service.tf
@@ -1,8 +1,9 @@
+# App Service Plan
 resource "azurerm_service_plan" "app_service_plan" {
   count               = !var.app_service_exists ? 1 : 0
   name                = "${var.app_name}-plan"
-  resource_group_name = azurerm_resource_group.rg[0].name
-  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = data.azurerm_resource_group.existing[0].name
+  location            = data.azurerm_resource_group.existing[0].location
   os_type             = "Linux"
   sku_name            = "Y1" # Consumption plan (pay-as-you-go) and can use containers
 }
@@ -11,8 +12,8 @@ resource "azurerm_service_plan" "app_service_plan" {
 resource "azurerm_linux_web_app" "api" {
   count               = !var.app_service_exists ? 1 : 0
   name                = "${var.app_name}-api"
-  resource_group_name = azurerm_resource_group.rg[0].name
-  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = data.azurerm_resource_group.existing[0].name
+  location            = data.azurerm_resource_group.existing[0].location
   service_plan_id     = azurerm_service_plan.app_service_plan[0].id
 
   site_config {
@@ -35,17 +36,17 @@ resource "azurerm_linux_web_app" "api" {
 
 # Output the App Service URL
 output "api_url" {
-  value = var.create_infrastructure && !var.app_service_exists ? "https://${azurerm_linux_web_app.api[0].default_hostname}" : "No new App Service created"
+  value = !var.app_service_exists ? "https://${azurerm_linux_web_app.api[0].default_hostname}" : "No new App Service created"
 }
 
 # Output the App Service name
 output "app_service_name" {
-  value = var.create_infrastructure && !var.app_service_exists ? azurerm_linux_web_app.api[0].name : (var.app_service_exists ? "${var.app_name}-api" : "No App Service")
+  value = !var.app_service_exists ? azurerm_linux_web_app.api[0].name : (var.app_service_exists ? "${var.app_name}-api" : "No App Service")
 }
 
 # Data source to get existing App Service if it exists
 data "azurerm_linux_web_app" "existing_api" {
   count               = var.app_service_exists ? 1 : 0
   name                = "${var.app_name}-api"
-  resource_group_name = azurerm_resource_group.rg[0].name
+  resource_group_name = data.azurerm_resource_group.existing[0].name
 }
